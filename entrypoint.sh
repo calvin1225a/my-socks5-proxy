422ebd32afb4ca66f1626af6f6a262f9b1ace299
calvin1225a <168755017+calvin1225a@users.noreply.github.com>
2025-10-29 22:35:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
-
-# 使用环境变量
 USERNAME="${PROXY_USER:-daqiang}"
-PASSWORD="${PROXY_PASSWORD:-summer125}"
-
-# 生成密码文件
-echo "$USERNAME:$PASSWORD" > /etc/danted.passwd
-
-# 动态获取容器 IP（Alpine busybox 兼容）
-CONTAINER_IP=$(hostname -i)
-
-# 替换配置中的 PLACEHOLDER 为实际 IP
-sed -i "s/external: PLACEHOLDER/external: $CONTAINER_IP/" /etc/danted.conf
-
-# 启动 sockd
+PASSWORD="${PROXY_PASSWORD:-summer1225}"
+# dante username 格式：user:pass 明文文件
+printf "%s:%s\n" "$USERNAME" "$PASSWORD" > /etc/danted.passwd
 exec sockd -f /etc/danted.conf -D
