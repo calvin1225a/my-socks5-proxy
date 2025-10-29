bf2dfade2e8c423c342cf0b208005b1e308d9fdd
calvin1225a <168755017+calvin1225a@users.noreply.github.com>
2025-10-29 20:55:28 +0800

    Update method to retrieve container IP address

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ PASSWORD="${PROXY_PASSWORD:-summer125}"
 # 生成密码文件
 echo "$USERNAME:$PASSWORD" > /etc/danted.passwd
 
-# 动态获取容器 IP（Alpine 兼容！）
-CONTAINER_IP=$(hostname -i)
+# 动态获取容器出口 IP（标准 Docker 方式，Alpine 兼容）
+CONTAINER_IP=$(ip route get 1 | awk '{print $7;exit}')
 
-# 替换 danted.conf 中的 external 为实际 IP（避免 wildcard）
+# 替换 danted.conf 中的 external 为实际 IP
 sed -i "s/external: eth0/external: $CONTAINER_IP/" /etc/danted.conf
 
 # 启动 sockd
