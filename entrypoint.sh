7892020c52ac57ee662f897e020e2422c5aaa080
calvin1225a <168755017+calvin1225a@users.noreply.github.com>
2025-10-29 20:20:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# 设置用户名和密码（请修改！）
+# 设置用户名和密码
 USERNAME="daqiang"
-PASSWORD="daqiang123456"  # ← 修改成你的强密码！
+PASSWORD="daqiang123456"  # ← 修改成你的密码！
 
-# 直接写明文密码文件（dante 支持明文）
+# 直接写明文密码文件
 echo "$USERNAME:$PASSWORD" > /etc/danted.passwd
 
-# 启动 danted
-exec danted -f /etc/danted.conf -D
+# 启动 sockd（Alpine 正确命令！）
+exec sockd -f /etc/danted.conf -D
