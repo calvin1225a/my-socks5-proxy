239382e53967d07fb8decedac9fe9950cbf346d0
calvin1225a <168755017+calvin1225a@users.noreply.github.com>
2025-10-29 20:34:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# 设置用户名和密码
-USERNAME="daqiang"
-PASSWORD="daqiang123456"  # ← 修改成你的密码！
+# 使用 Railway 环境变量
+USERNAME="${PROXY_USER:-daqiang}"
+PASSWORD="${PROXY_PASSWORD:-summer125}"
 
-# 直接写明文密码文件
+# 生成密码文件
 echo "$USERNAME:$PASSWORD" > /etc/danted.passwd
 
-# 启动 sockd（Alpine 正确命令！）
+# 启动 sockd
 exec sockd -f /etc/danted.conf -D
